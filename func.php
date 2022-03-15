79aee86d7e7b97b2796134834ab1841d870b957d
satyasandeep165 <88207329+satyasandeep165@users.noreply.github.com>
2022-03-15 23:08:39 +0530

    Add files via upload

---
+++ func.php
@@ -0,0 +1,23 @@
+<?php
+$con=mysqli_connect("localhost","root","","hmsdb");
+if(isset($_POST['login_submit'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $query="select * from logintb where username='$username' and password='$password'";
+    $result=mysqli_query($con,$query);
+    if(mysqli_num_rows($result)==1)
+    {
+        header("Location:admin-panel.php");
+    }
+    else
+    {
+        echo "<script>alert('Invalid details')</script>";
+        echo "<script>window.open('index.php','_self')</script>";
+    }
+
+}
+
+
+
+
+?>
